3a9c6dcb2d562abc0d25635bb0941998654c530d
ZeeCoder91 <zeenavatman@gmail.com>
2021-11-02 12:10:25 -0400

    Finished first porject

--- script.js
+++ script.js
@@ -11,8 +11,51 @@ form.addEventListener('submit',(e) => {
 
 function checkInputs(){
     // get the values from the inputs
-    const userNameValue = username.value.trim();
+    const usernameValue = username.value.trim();
     const emailValue = email.value.trim();
     const passwordValue = password.value.trim();
     const password2Value = password2.value.trim();
+
+    if(usernameValue === '') {
+        setErrorFor(username, 'Username cannot be blank');
+    } else {
+        //add success class
+        setSuccessFor(username);
+    }
+
+    if(emailValue === '') {
+        setErrorFor(email, 'Email cannot be blank');
+    } else {
+        setSuccessFor(email);
+    }
+
+    if(passwordValue === '') {
+        setErrorFor(password, 'Password cannot be blank');
+    } else {
+        setSuccessFor(password);
+    }
+
+    if(password2Value === '') {
+        setErrorFor(password2, 'Password Check cannot be blank!');
+    } else if(password2Value !== passwordValue) {
+        setErrorFor(password2, 'Passwords do not match!');
+    } else {
+        setSuccessFor(password2);
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement; //.form-control
+    const small = formControl.querySelector('small');
+
+    // add error message inside small tag
+    small.innerText = message;
+
+    // add error class
+    formControl.className = 'form-control error';
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement; 
+    formControl.className = 'form-control success';
 }
